66c8aa90d1ceb485a91e6f72206f81a6e2e79e30
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-03 20:05:40 +0100

    Proyecto reecreado, Header creado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/LoginView.vue'
-
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
     {
       path: '/login',
       name: 'login',
--- src/stores/dtos/UserDto.ts
+++
@@ -1,7 +0,0 @@
-export interface UserDto {
-    id: number
-    name: string
-    lastName: string
-    birthDate: Date
-    phoneNumber: string,
-}
